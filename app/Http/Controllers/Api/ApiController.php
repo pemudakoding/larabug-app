6659216b0ecfe88e4a98631ed16809b35aa6d737
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-08-19 15:05:45 +0200

    Removed spaces

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -28,7 +28,7 @@ class ApiController extends Controller
                 'error' => 'Did not receive the correct parameters to process this exception'
             ])->setStatusCode(422);
         }
-        
+
         dispatch_sync(new ProcessException([
             'id' => $id = Uuid::uuid4(),
             'host' => array_get($request->input('exception'), 'host'),
