a78a782c5e4dc210609a17b2a1b2e7787e3b939d
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-08-20 15:20:44 +0200

    Only show env when exists in DB

--- resources/assets/js/panel/Pages/Exceptions/Show.vue
+++ resources/assets/js/panel/Pages/Exceptions/Show.vue
@@ -12,7 +12,7 @@
     <div class="flex flex-col sm:flex-row flex-1">
         <aside class="w-full sm:w-1/4 sm:h-full bg-white border-r border-gray-200">
             <dl class="px-6 py-4 space-y-4">
-                <div>
+                <div v-if="exception.environment">
                     <dt class="text-sm font-medium">Environment</dt>
                     <dd>
                       <Code>{{ exception.environment }}</Code>
