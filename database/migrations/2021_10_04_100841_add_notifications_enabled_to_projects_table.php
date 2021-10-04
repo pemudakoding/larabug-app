9a7ffc4f1dcb9ed493d9d885d047d1cc7e3af4e0
Rick Lambrechts <rick@rl-webdiensten.nl>
2021-10-04 16:26:27 +0200

    Updated migrations file to have notification fields.

---
+++ database/migrations/2021_10_04_100841_add_notifications_enabled_to_projects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotificationsEnabledToProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->boolean('notifications_enabled')->default(true)->after('receive_email');
+            $table->boolean('custom_webhook_enabled')->default(false)->after('notifications_enabled');
+            $table->boolean('discord_webhook_enabled')->default(false)->after('notifications_enabled');
+            $table->boolean('slack_webhook_enabled')->default(false)->after('notifications_enabled');
+            $table->boolean('mobile_notifications_enabled')->default(true)->after('notifications_enabled');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('notifications_enabled');
+            $table->dropColumn('custom_webhook_enabled');
+            $table->dropColumn('discord_webhook_enabled');
+            $table->dropColumn('slack_webhook_enabled');
+            $table->dropColumn('mobile_notifications_enabled');
+        });
+    }
+}
