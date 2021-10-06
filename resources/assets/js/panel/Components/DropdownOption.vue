179f7a5fc8e54159046e8928bae756453d6fb748
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-10-06 17:35:47 +0200

    Added a toolbar to the exceptions details page
    - Added toolbar
    - Added Dropdown and DropdownOption components

---
+++ resources/assets/js/panel/Components/DropdownOption.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <button
+        @click="$emit('click', $event)"
+        class="flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:bg-primary-600 hover:text-white focus:bg-primary-700 focus:text-white group"
+        type="button">
+      <span><slot /></span>
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "DropdownOption",
+  emits: [
+      'click',
+  ],
+}
+</script>
+
+<style scoped>
+
+</style>
