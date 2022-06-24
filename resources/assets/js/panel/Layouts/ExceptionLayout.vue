abe5f528e9562c08af4ddb47961c6c95a9558608
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-06-24 10:30:52 +0200

    issue nav icon for exception layout

--- resources/assets/js/panel/Layouts/ExceptionLayout.vue
+++ resources/assets/js/panel/Layouts/ExceptionLayout.vue
@@ -52,6 +52,26 @@
             <span class="md:hidden">Projects</span>
           </NavListItem>
 
+          <NavListItem @click="open = false" href="/panel/issues" :is-active="route().current('panel.issues.*')" :big="false">
+            <template #icon>
+              <svg
+                  class="w-6 h-6 text-primary-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+              </svg>
+            </template>
+
+            <span class="md:hidden">Issues</span>
+          </NavListItem>
+
           <NavListItem @click="open = false" href="/panel/profile" :is-active="route().current('panel.profile.*')" :big="false">
             <template #icon>
               <svg
